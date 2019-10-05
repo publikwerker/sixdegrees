56b1179e9d2da43f0793d0220bcda69daa80aef2
Publikwerker <hello@publikwerks.com>
2019-10-04 20:39:10 -0700

    added basic styling to form component

--- src/components/Body.js
+++ src/components/Body.js
@@ -78,6 +78,7 @@ export default class Body extends React.Component {
             <span className="body__form-input--label">
             How many iterations would you like to see?</span>
             <input type="number"
+            className="input"
             name="body__form-input--iteration"
             placeholder=" 1 or 5, for example"
             onChange={this.handleChange} />
