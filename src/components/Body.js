2ee481d94f1c5553a0387d731ce4c17809018718
Publikwerker <hello@publikwerks.com>
2019-10-04 17:34:28 -0700

    set up handleSubmit and created getSequence in prep to send and receive from API

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,6 +4,8 @@ export default class Body extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      iterations: 0,
+      sequence: [],
       error: null
     };
   }
@@ -11,17 +13,35 @@ export default class Body extends React.Component {
   setError = (error) => {
     console.log("setError ran");
     this.setState({
+      iterations: 0,
+      sequence: [],
       error
     })
+  }
+
+  handleChange = async (event) => {
+    try {
+      this.setState({
+        iterations: event.target.value,
+        sequence: [],
+        error: null,
+      })
+    } catch (error) {
+      this.setError(error.message);
+    }
   }
 
   handleSubmit = async (event) => {
     try {
       event.preventDefault();
-      console.log("handle submit ran");
+      this.getSequence(this.state.iterations);
     } catch (error) {
       this.setError(error.message);
     }
+  }
+
+  getSequence = async (number) => {
+    console.log(number);
   }
 
   render () {
@@ -40,6 +60,7 @@ export default class Body extends React.Component {
             placeholder=" 1 or 5, for example"
             onChange={this.handleChange} />
           </label>
+          <input type="submit" className="button" value="Submit" />
         </form>
       </div>
     )
